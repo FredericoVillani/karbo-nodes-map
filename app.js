241516511f297b9925be5bdd9d5b36f279068556
FredericoVillani <34401583+FredericoVillani@users.noreply.github.com>
2018-07-19 18:57:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const locations = require('./locations');
 
 locations.cacheLocations();
-setInterval(locations.cacheLocations, 36e5); // 1hr
+setInterval(locations.cacheLocations, 140000); // next to 3 minutes
 
 app.set('view engine', 'ejs');
 app.set("views", __dirname + "/views");
@@ -10,6 +10,7 @@ app.set("views", __dirname + "/views");
 // blog home page
 app.get('/', async (req, res) => {
     res.render('home', { locations: await locations.getCachedLocations() });
+    setInterval(locations.getCachedLocations, 12000); // 20 seconds
 });
 
 
